8b669459b801151cd1b4bc45758b03e067512ee1
thalvari <tuomas.halvari@helsinki.fi>
2020-04-27 16:16:00 +0300

    modify automl.py

--- automl.py
+++ automl.py
@@ -1,4 +1,5 @@
 import pickle
+import pprint
 
 import autokeras as ak
 import numpy as np
@@ -10,7 +11,13 @@ with open("train/preprocessed_docs.pkl", "rb") as f:
     x_train = np.array(pickle.load(f))
 y_train = np.load("train/labels.npy")
 
-clf = ak.TextClassifier(max_trials=100, multi_label=True, seed=seed)
+print(x_train.shape)
+print(y_train.shape)
+
+clf = ak.TextClassifier(max_trials=100, multi_label=True, seed=seed, overwrite=True, directory="tmp")
 clf.fit(x_train, y_train)
 
-print(clf.export_model().summary())
+model = clf.export_model()
+print(type(model))
+model.summary(line_length=120)
+pprint.PrettyPrinter(indent=2).pprint(model.get_config())
