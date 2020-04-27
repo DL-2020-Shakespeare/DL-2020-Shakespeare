6a91ee1b22c6caca69bd4e668cf1c9a838f46791
thalvari <tuomas.halvari@helsinki.fi>
2020-04-27 16:45:15 +0300

    small changes

--- automl.py
+++ automl.py
@@ -1,3 +1,4 @@
+import argparse
 import pickle
 import pprint
 
@@ -7,17 +8,32 @@ import numpy as np
 seed = 42
 np.random.seed(seed)
 
-with open("train/preprocessed_docs.pkl", "rb") as f:
-    x_train = np.array(pickle.load(f))
-y_train = np.load("train/labels.npy")
 
-print(x_train.shape)
-print(y_train.shape)
+def main(args):
+    n_docs = args.n_docs
 
-clf = ak.TextClassifier(max_trials=100, multi_label=True, seed=seed, overwrite=True, directory="tmp")
-clf.fit(x_train, y_train)
+    with open("train/preprocessed_docs.pkl", "rb") as f:
+        x_train = np.array(pickle.load(f))
+    y_train = np.load("train/labels.npy")
 
-model = clf.export_model()
-print(type(model))
-model.summary(line_length=120)
-pprint.PrettyPrinter(indent=2).pprint(model.get_config())
+    if n_docs is not None:
+        x_train = x_train[:n_docs]
+        y_train = y_train[:n_docs]
+
+    print(x_train.shape)
+    print(y_train.shape)
+
+    clf = ak.TextClassifier(max_trials=100, multi_label=True, seed=seed, overwrite=True, directory="tmp")
+    clf.fit(x_train, y_train)
+
+    model = clf.export_model()
+    print(type(model))
+    model.summary(line_length=120)
+    pprint.PrettyPrinter(indent=2).pprint(model.get_config())
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--n_docs", type=int)
+    args = parser.parse_args()
+    main(args)
