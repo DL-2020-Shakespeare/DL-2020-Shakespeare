83b0576757e5a95b1ca4c1e765e72c2c2d22cb1e
thalvari <tuomas.halvari@helsinki.fi>
2020-05-01 14:16:53 +0300

    modify automl.py

--- automl.py
+++ automl.py
@@ -8,7 +8,8 @@ warnings.filterwarnings("ignore")
 import autokeras as ak
 import numpy as np
 import pandas as pd
-from sklearn.metrics import average_precision_score
+from sklearn.metrics import average_precision_score, accuracy_score, \
+    label_ranking_average_precision_score, ndcg_score, f1_score
 from sklearn.model_selection import train_test_split
 
 seed = 42
@@ -41,9 +42,21 @@ def main(args):
     clf.fit(np.array(x_train), y_train)
 
     for model in clf.tuner.get_best_models(10):
-        y_pred = model.predict(x_test)
-        print(average_precision_score(y_test, y_pred, average="micro"))
-        print(average_precision_score(y_test, y_pred, average="weighted"))
+        y_pred_prob = model.predict(x_test)
+        y_pred = np.round(y_pred_prob)
+
+        print(f"accuracy: {accuracy_score(y_test, y_pred)}")
+        print(f"AP (macro): {average_precision_score(y_test, y_pred_prob, average='macro')}")
+        print(f"AP (micro): {average_precision_score(y_test, y_pred_prob, average='micro')}")
+        print(f"AP (samples): {average_precision_score(y_test, y_pred_prob, average='samples')}")
+        print(f"AP (weighted): {average_precision_score(y_test, y_pred_prob, average='weighted')}")
+        print(f"F1 (macro): {f1_score(y_test, y_pred, average='macro')}")
+        print(f"F1 (micro): {f1_score(y_test, y_pred, average='micro')}")
+        print(f"F1 (samples): {f1_score(y_test, y_pred, average='samples')}")
+        print(f"F1 (weighted): {f1_score(y_test, y_pred, average='weighted')}")
+        print(f"LRAP: {label_ranking_average_precision_score(y_test, y_pred_prob)}")
+        print(f"NDCG: {ndcg_score(y_test, y_pred_prob)}")
+
         model.summary(line_length=120)
         pprint.PrettyPrinter(indent=2).pprint(model.get_config())
 
