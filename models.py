b3bfcba1940ed6a6b3524a5372c085a4608199fe
thalvari <tuomas.halvari@helsinki.fi>
2020-05-21 09:01:23 +0300

    add kwargs to models

--- models.py
+++ models.py
@@ -1,7 +1,3 @@
-import warnings
-
-warnings.filterwarnings("ignore")
-
 import numpy as np
 import tensorflow as tf
 from tensorflow.distribute import MirroredStrategy
@@ -13,7 +9,9 @@ from tensorflow.keras.layers import Dense, Dropout, Activation, Embedding, Conv1
 from tensorflow.keras.models import Sequential
 
 
-def init_cnn_1(embedding_matrix, n_vocabulary, n_embedding, n_sequence, n_labels, loss):
+def init_cnn_1(embedding_matrix, n_vocabulary, n_embedding, n_sequence, n_labels, **kwargs):
+    loss = kwargs["loss"]
+
     with MirroredStrategy().scope():
         model = Sequential()
 
@@ -59,7 +57,9 @@ def init_cnn_1(embedding_matrix, n_vocabulary, n_embedding, n_sequence, n_labels
         return model
 
 
-def init_cnn_2(embedding_matrix, n_vocabulary, n_embedding, n_sequence, n_labels, loss):
+def init_cnn_2(embedding_matrix, n_vocabulary, n_embedding, n_sequence, n_labels, **kwargs):
+    loss = kwargs["loss"]
+
     with MirroredStrategy().scope():
         model = Sequential()
 
@@ -108,26 +108,9 @@ def init_cnn_2(embedding_matrix, n_vocabulary, n_embedding, n_sequence, n_labels
         return model
 
 
-def init_bi_lstm_1(embedding_matrix, n_vocabulary, n_embedding, n_sequence, n_labels, loss):
-    with MirroredStrategy().scope():
-        model = Sequential()
+def init_bi_lstm_2(embedding_matrix, n_vocabulary, n_embedding, n_sequence, n_labels, **kwargs):
+    loss = kwargs["loss"]
 
-        model.add(Embedding(
-            input_dim=n_vocabulary,
-            output_dim=n_embedding,
-            embeddings_initializer=Constant(embedding_matrix),
-            input_length=n_sequence,
-            trainable=False
-        ))
-
-        model.add(Bidirectional(LSTM(128, dropout=.2)))
-
-        model.add(Dense(n_labels, activation="sigmoid"))
-        model.compile(loss=loss, optimizer="adam")
-        return model
-
-
-def init_bi_lstm_2(embedding_matrix, n_vocabulary, n_embedding, n_sequence, n_labels, loss):
     with MirroredStrategy().scope():
         model = Sequential()
 
@@ -146,7 +129,11 @@ def init_bi_lstm_2(embedding_matrix, n_vocabulary, n_embedding, n_sequence, n_la
         return model
 
 
-def init_cnn_bi_lstm_1(embedding_matrix, n_vocabulary, n_embedding, n_sequence, n_labels, loss):
+def init_cnn_bi_lstm_1(embedding_matrix, n_vocabulary, n_embedding, n_sequence, n_labels, **kwargs):
+    filters_1 = kwargs["filters_1"]
+    filters_2 = kwargs["filters_2"]
+    loss = kwargs["loss"]
+
     with MirroredStrategy().scope():
         model = Sequential()
 
@@ -158,13 +145,13 @@ def init_cnn_bi_lstm_1(embedding_matrix, n_vocabulary, n_embedding, n_sequence, 
             trainable=False
         ))
 
-        model.add(Conv1D(400, 2))
+        model.add(Conv1D(filters_1, 2))
         model.add(BatchNormalization())
         model.add(ReLU())
         model.add(MaxPooling1D(2))
         model.add(Dropout(.2))
 
-        model.add(Conv1D(500, 2))
+        model.add(Conv1D(filters_2, 2))
         model.add(BatchNormalization())
         model.add(ReLU())
         model.add(MaxPooling1D(2))
@@ -175,275 +162,3 @@ def init_cnn_bi_lstm_1(embedding_matrix, n_vocabulary, n_embedding, n_sequence, 
         model.add(Dense(n_labels, activation="sigmoid"))
         model.compile(loss=loss, optimizer="adam")
         return model
-
-
-def init_cnn_bi_lstm_2(embedding_matrix, n_vocabulary, n_embedding, n_sequence, n_labels, loss):
-    with MirroredStrategy().scope():
-        model = Sequential()
-
-        model.add(Embedding(
-            input_dim=n_vocabulary,
-            output_dim=n_embedding,
-            embeddings_initializer=Constant(embedding_matrix),
-            input_length=n_sequence,
-            trainable=False
-        ))
-
-        model.add(Conv1D(512, 2))
-        model.add(BatchNormalization())
-        model.add(ReLU())
-        model.add(MaxPooling1D(2))
-        model.add(Dropout(.2))
-
-        model.add(Conv1D(512, 2))
-        model.add(BatchNormalization())
-        model.add(ReLU())
-        model.add(MaxPooling1D(2))
-        model.add(Dropout(.2))
-
-        model.add(Bidirectional(LSTM(256, dropout=.2)))
-
-        model.add(Dense(n_labels, activation="sigmoid"))
-        model.compile(loss=loss, optimizer="adam")
-        return model
-
-
-def init_cnn_bi_lstm_3(embedding_matrix, n_vocabulary, n_embedding, n_sequence, n_labels, loss):
-    with MirroredStrategy().scope():
-        model = Sequential()
-
-        model.add(Embedding(
-            input_dim=n_vocabulary,
-            output_dim=n_embedding,
-            embeddings_initializer=Constant(embedding_matrix),
-            input_length=n_sequence,
-            trainable=False
-        ))
-
-        model.add(Conv1D(512, 2))
-        model.add(BatchNormalization())
-        model.add(ReLU())
-        model.add(MaxPooling1D(2))
-        model.add(Dropout(.2))
-
-        model.add(Conv1D(512, 2))
-        model.add(BatchNormalization())
-        model.add(ReLU())
-        model.add(MaxPooling1D(2))
-        model.add(Dropout(.2))
-
-        model.add(Conv1D(512, 2))
-        model.add(BatchNormalization())
-        model.add(ReLU())
-        model.add(MaxPooling1D(2))
-        model.add(Dropout(.2))
-
-        model.add(Bidirectional(LSTM(256, dropout=.2)))
-
-        model.add(Dense(n_labels, activation="sigmoid"))
-        model.compile(loss=loss, optimizer="adam")
-        return model
-
-
-def init_cnn_bi_lstm_4(embedding_matrix, n_vocabulary, n_embedding, n_sequence, n_labels, loss):
-    with MirroredStrategy().scope():
-        model = Sequential()
-
-        model.add(Embedding(
-            input_dim=n_vocabulary,
-            output_dim=n_embedding,
-            embeddings_initializer=Constant(embedding_matrix),
-            input_length=n_sequence,
-            trainable=False
-        ))
-
-        model.add(Conv1D(512, 2))
-        model.add(BatchNormalization())
-        model.add(ReLU())
-        model.add(MaxPooling1D(2))
-        model.add(Dropout(.2))
-
-        model.add(Bidirectional(LSTM(256, dropout=.2)))
-
-        model.add(Dense(n_labels, activation="sigmoid"))
-        model.compile(loss=loss, optimizer="adam")
-        return model
-
-
-def init_cnn_bi_lstm_5(embedding_matrix, n_vocabulary, n_embedding, n_sequence, n_labels, loss):
-    with MirroredStrategy().scope():
-        model = Sequential()
-
-        model.add(Embedding(
-            input_dim=n_vocabulary,
-            output_dim=n_embedding,
-            embeddings_initializer=Constant(embedding_matrix),
-            input_length=n_sequence,
-            trainable=False
-        ))
-
-        model.add(Conv1D(400, 3))
-        model.add(BatchNormalization())
-        model.add(ReLU())
-        model.add(MaxPooling1D(2))
-        model.add(Dropout(.2))
-
-        model.add(Conv1D(500, 3))
-        model.add(BatchNormalization())
-        model.add(ReLU())
-        model.add(MaxPooling1D(2))
-        model.add(Dropout(.2))
-
-        model.add(Bidirectional(LSTM(256, dropout=.2)))
-
-        model.add(Dense(n_labels, activation="sigmoid"))
-        model.compile(loss=loss, optimizer="adam")
-        return model
-
-
-def init_cnn_bi_lstm_6(embedding_matrix, n_vocabulary, n_embedding, n_sequence, n_labels, loss):
-    with MirroredStrategy().scope():
-        model = Sequential()
-
-        model.add(Embedding(
-            input_dim=n_vocabulary,
-            output_dim=n_embedding,
-            embeddings_initializer=Constant(embedding_matrix),
-            input_length=n_sequence,
-            trainable=False
-        ))
-
-        model.add(Conv1D(400, 2))
-        model.add(BatchNormalization())
-        model.add(ReLU())
-        model.add(MaxPooling1D(2))
-        model.add(Dropout(.2))
-
-        model.add(Bidirectional(LSTM(256, dropout=.2)))
-
-        model.add(Dense(n_labels, activation="sigmoid"))
-        model.compile(loss=loss, optimizer="adam")
-        return model
-
-
-def init_cnn_bi_lstm_7(embedding_matrix, n_vocabulary, n_embedding, n_sequence, n_labels, loss):
-    with MirroredStrategy().scope():
-        model = Sequential()
-
-        model.add(Embedding(
-            input_dim=n_vocabulary,
-            output_dim=n_embedding,
-            embeddings_initializer=Constant(embedding_matrix),
-            input_length=n_sequence,
-            trainable=False
-        ))
-
-        model.add(Conv1D(400, 2))
-        model.add(BatchNormalization())
-        model.add(ReLU())
-        model.add(MaxPooling1D(2))
-        model.add(Dropout(.2))
-
-        model.add(Conv1D(500, 2))
-        model.add(BatchNormalization())
-        model.add(ReLU())
-        model.add(MaxPooling1D(2))
-        model.add(Dropout(.2))
-
-        model.add(Bidirectional(LSTM(512, dropout=.2)))
-
-        model.add(Dense(n_labels, activation="sigmoid"))
-        model.compile(loss=loss, optimizer="adam")
-        return model
-
-
-def init_cnn_bi_lstm_8(embedding_matrix, n_vocabulary, n_embedding, n_sequence, n_labels, loss):
-    with MirroredStrategy().scope():
-        model = Sequential()
-
-        model.add(Embedding(
-            input_dim=n_vocabulary,
-            output_dim=n_embedding,
-            embeddings_initializer=Constant(embedding_matrix),
-            input_length=n_sequence,
-            trainable=False
-        ))
-
-        model.add(Conv1D(400, 2))
-        model.add(BatchNormalization())
-        model.add(ReLU())
-        model.add(MaxPooling1D(2))
-        model.add(Dropout(.2))
-
-        model.add(Conv1D(500, 2))
-        model.add(BatchNormalization())
-        model.add(ReLU())
-        model.add(MaxPooling1D(2))
-        model.add(Dropout(.2))
-
-        model.add(Bidirectional(LSTM(128, dropout=.2)))
-
-        model.add(Dense(n_labels, activation="sigmoid"))
-        model.compile(loss=loss, optimizer="adam")
-        return model
-
-
-def init_cnn_lstm_1(embedding_matrix, n_vocabulary, n_embedding, n_sequence, n_labels, loss):
-    with MirroredStrategy().scope():
-        model = Sequential()
-
-        model.add(Embedding(
-            input_dim=n_vocabulary,
-            output_dim=n_embedding,
-            embeddings_initializer=Constant(embedding_matrix),
-            input_length=n_sequence,
-            trainable=False
-        ))
-
-        model.add(Conv1D(400, 2))
-        model.add(BatchNormalization())
-        model.add(ReLU())
-        model.add(MaxPooling1D(2))
-        model.add(Dropout(.2))
-
-        model.add(Conv1D(500, 2))
-        model.add(BatchNormalization())
-        model.add(ReLU())
-        model.add(MaxPooling1D(2))
-        model.add(Dropout(.2))
-
-        model.add(LSTM(256, dropout=.2))
-
-        model.add(Dense(n_labels, activation="sigmoid"))
-        model.compile(loss=loss, optimizer="adam")
-        return model
-
-
-def init_cnn_bi_gru_1(embedding_matrix, n_vocabulary, n_embedding, n_sequence, n_labels, loss):
-    model = Sequential()
-
-    model.add(Embedding(
-        input_dim=n_vocabulary,
-        output_dim=n_embedding,
-        embeddings_initializer=Constant(embedding_matrix),
-        input_length=n_sequence,
-        trainable=False
-    ))
-
-    model.add(Conv1D(400, 2))
-    model.add(BatchNormalization())
-    model.add(ReLU())
-    model.add(MaxPooling1D(2))
-    model.add(Dropout(.2))
-
-    model.add(Conv1D(500, 2))
-    model.add(BatchNormalization())
-    model.add(ReLU())
-    model.add(MaxPooling1D(2))
-    model.add(Dropout(.2))
-
-    model.add(Bidirectional(GRU(256, dropout=.2)))
-
-    model.add(Dense(n_labels, activation="sigmoid"))
-    model.compile(loss=loss, optimizer="adam")
-    return model
